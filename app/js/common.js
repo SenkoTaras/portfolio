9a1215b65a0f8f2060c1f2df300b78bf1d98adb2
Taras <taras.senko.99@gmail.com>
2020-03-01 01:26:28 +0200

    v 0.5 +blog +portfolio

--- app/js/common.js
+++ app/js/common.js
@@ -1,7 +1,25 @@
-$(document).ready(function(){
-    $("main").onepage_scroll({
-      sectionContainer: "section",
-      responsiveFallback: 600,
-      loop: true
-    });
-      });
+$(document).ready(function () {
+  $("main").onepage_scroll({
+    sectionContainer: "section",
+    responsiveFallback: 600,
+    loop: true
+  });
+});
+
+$(document).ready(function () {
+  $(".setsize").each(function () {
+    $(this).height($(this).width());
+  });
+});
+$(window).on('resize', function () {
+  $(".setsize").each(function () {
+    $(this).height($(this).width());
+  });
+});
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin: 0,
+  nav:true,
+  items: 1
+});
