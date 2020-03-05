adf3c120a560e8af8753ddb159bf2b9a8369a8fb
Taras <taras.senko.99@gmail.com>
2020-03-06 00:07:39 +0200

    +mobil +menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,25 +1,39 @@
+jQuery(document).ready(function($) {
+
 $(document).ready(function () {
   $("main").onepage_scroll({
     sectionContainer: "section",
-    responsiveFallback: 600,
-    loop: true
+    updateURL:  true,
+    loop: true,
+    responsiveFallback: 600
   });
+
 });
 
-$(document).ready(function () {
-  $(".setsize").each(function () {
-    $(this).height($(this).width());
-  });
-});
-$(window).on('resize', function () {
-  $(".setsize").each(function () {
-    $(this).height($(this).width());
-  });
-});
+
+
 
 $('.owl-carousel').owlCarousel({
   loop:true,
   margin: 0,
   nav:true,
   items: 1
+});
+
+
+
+  var bodyWidth = $('body').width();
+  if (bodyWidth < 600) {
+      let linkMenu = document.querySelectorAll('.nav_link');
+      linkMenu[0].href = '#home';
+      linkMenu[1].href = '#about';
+      linkMenu[2].href = '#portfolio';
+      linkMenu[3].href = '#blog';
+      linkMenu[4].href = '#contacts';
+  } 
+  
+
+
+
+
 });
